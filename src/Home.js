1658a1baf0127466450ae722fa7d9e89f36dcd42
Salim DELLALI <dellali.salim@gmail.com>
2020-10-13 23:09:09 +0100

    created the json files for speakers and schedule, generated speakers using the Speaker component

--- src/Home.js
+++ src/Home.js
@@ -1,40 +1,12 @@
 import React from 'react'
 import logo from './logo.svg';
 import { Row, Col, Card } from 'react-bootstrap';
-import faker from 'faker';
-import { FaTwitterSquare } from "react-icons/fa";
-import { GoLinkExternal } from "react-icons/go";
+import { Speaker } from './components/Speaker'
+
+import speakers from './speakers.json';
 
 export const Home = () => {
     
-     const getSpeakers = () => {
-        const speakers = []
-        for (let i=0; i<4; i++) {
-             
-            let uuid = faker.random.uuid();
-
-            let contextualCard = faker.helpers.contextualCard();
-            let avatar = contextualCard.avatar;
-            let name = contextualCard.name;
-            let username = contextualCard.username;
-            let twitterLink = "https://twitter.com/" + username;
-           
-            let speaker = 
-            <Col key={uuid} md={3}>
-                <Card>
-                    <Card.Img variant="top" src={avatar}/>
-                    <Card.Body className="align-items-center">
-                        <Card.Title>{name}</Card.Title>
-                        <Card.Text><a href={twitterLink} target="_blank" rel="noopener noreferrer"><FaTwitterSquare /> @{username} <GoLinkExternal /></a></Card.Text>
-                    </Card.Body>
-                </Card>
-            </Col>
-            speakers.push(speaker);
-        }
-        console.log(speakers);
-        return speakers;
-    }
-
     return (
         <>
             <Row className="mb-4">
@@ -54,12 +26,13 @@ export const Home = () => {
                 </Col>
             </Row>
 
-            <Row className="mb-4">
-                    {getSpeakers()}  
-            </Row>
-
-            <Row className="mb-4">          
-                {getSpeakers()} 
+            <Row>  
+            {speakers.map((speakerDetails) => {
+                return <Speaker 
+                            key={speakerDetails.id} 
+                            speakerDetails={speakerDetails}
+                        />
+            })}
             </Row>
         </>
     )
