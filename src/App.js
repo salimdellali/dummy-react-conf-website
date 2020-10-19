89ed25d7fd3283ca93807113294e6064984a67d4
Salim DELLALI <dellali.salim@gmail.com>
2020-10-19 12:13:28 +0100

    same previous commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container } from 'react-bootstrap';
 import './App.css';
 
 // import the website pages
